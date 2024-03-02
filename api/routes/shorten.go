4035a14e55898517c143e69d9aea320dad1155fd
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-03-02 14:53:23 +0630

    update(): remove comments

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -51,31 +51,20 @@ func ShortenURL(c *fiber.Ctx) error {
 		}
 	}
 
-	// check if the input is an actual URL
 	if !govalidator.IsURL(body.URL) {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "Invalid URL",
 		})
 	}
 
-	// check for the domain error
-	// users may abuse the shortener by shorting the domain `localhost:3000` itself
-	// leading to a inifite loop, so don't accept the domain for shortening
 	if !helpers.RemoveDomainError(body.URL) {
 		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
 			"error": "haha... nice try",
 		})
 	}
 
-	// enforce https
-	// all url will be converted to https before storing in database
 	body.URL = helpers.EnforceHTTP(body.URL)
 
-	// check if the user has provided any custom dhort urls
-	// if yes, proceed,
-	// else, create a new short using the first 6 digits of uuid
-	// haven't performed any collision checks on this
-	// you can create one for your own
 	var id string
 	if body.CustomShort == "" {
 		id = uuid.New().String()[:6]
@@ -87,14 +76,13 @@ func ShortenURL(c *fiber.Ctx) error {
 	defer r.Close()
 
 	val, _ = r.Get(database.Ctx, id).Result()
-	// check if the user provided short is already in use
 	if val != "" {
 		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
 			"error": "URL short already in use",
 		})
 	}
 	if body.Expiry == 0 {
-		body.Expiry = 24 // default expiry of 24 hours
+		body.Expiry = 24
 	}
 	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
 	if err != nil {
@@ -102,7 +90,6 @@ func ShortenURL(c *fiber.Ctx) error {
 			"error": "Unable to connect to server",
 		})
 	}
-	// respond with the url, short, expiry in hours, calls remaining and time to reset
 	resp := response{
 		URL:             body.URL,
 		CustomShort:     "",
